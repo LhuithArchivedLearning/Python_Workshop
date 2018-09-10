9e99267a3557252d754f088ff25f8a7af3166464
Togene <jhonnywhiplash@hotmail.com.au>
2018-09-11 00:57:39 +1000

    Added: Week 7 and 8

--- Workshop06-Questions/homework_questions/1-shopping_list_Combobox.py
+++ Workshop06-Questions/homework_questions/1-shopping_list_Combobox.py
@@ -30,7 +30,7 @@
 #
 
 # Import the necessary Tkinter functions
-from tkinter import Tk, Text, Button, END
+from tkinter import Tk, Text, Button, END, Label
 from tkinter.ttk import Combobox
 
 # Create a window
@@ -50,25 +50,36 @@ groceries = ['Eggs', 'Milk', 'Bread', 'Coffee', 'Tea', 'Lettuce',
 # 1. Define a function to display the current choice
 #    in the Text area when the button is pushed
 
-def display_current_choice(event, text):
-    Choice.delete(1.0, END);
-    Choice.insert(END, text);
-    
+def display_current_choice(cBox):
+    if len(Choice.get("1.0", END)) == 1:
+        Choice.insert(END,  cBox.get());
+        FinishButton['text'] = "CheckOut";        
+    else:
+        Choice.insert(END,  ", " + cBox.get());
+
+def checkout():
+    if len(Choice.get("1.0", END)) != 1:
+        Choice.delete("1.0", END);
+        FinishButton['text'] = "Items Added";
+        
+CartLabel = Label(window, height = 1, width = 30, font = ('Arial', 12), text = 'Cart');
+CartLabel.pack();
 # 2. Create a Text area to display the user's choice and
 #    pack it into the main window
-Choice = Text(window, height = 2, width = 10, font = ('Arial', 32));
+Choice = Text(window, height = 20, width = 30, font = ('Arial', 12));
 Choice.pack();
 
 # 3. Create a Combobox widget whose parent container is the
 #    main window and pack it
-grossCombo = Combobox(window, width = 10, value = groceries);
-grossCombo.current(0);
+grossCombo = Combobox(window, width = 20, value = groceries);
+grossCombo.set("Please Select Yo Shizz");
 grossCombo.state(['readonly']);
-grossCombo.bind("<<ComboboxSelected>>", lambda event:display_current_choice(event, grossCombo.get()));
+grossCombo.bind("<<ComboboxSelected>>", lambda event:display_current_choice(grossCombo));
 grossCombo.pack();
 # 4. Create a Button to push when the user is happy with
 #    their choice and pack it
-
+FinishButton = Button(window, height = 1, width = 30, font = ('Arial', 12), text = 'CheckOut', command = checkout);
+FinishButton.pack();
 
 # Start the event loop to react to user inputs
 window.mainloop()
--- Workshop06-Questions/homework_questions/2-shopping_list_saveable.py
+++ Workshop06-Questions/homework_questions/2-shopping_list_saveable.py
@@ -22,7 +22,7 @@
 #
 
 # Import the necessary Tkinter functions
-from tkinter import Tk, Text, Button, END
+from tkinter import Tk, Text, Button, END, Label
 from tkinter.ttk import Combobox
 
 # Create a window
@@ -38,14 +38,41 @@ groceries = ['Eggs', 'Milk', 'Bread', 'Coffee', 'Tea', 'Lettuce',
 
 
 ##### DEVELOP YOUR SOLUTION HERE, BASED ON YOUR PREVIOUS SHOPPING LIST CODE
+def display_current_choice(cBox):
+    if len(Choice.get("1.0", END)) == 1:
+        Choice.insert(END,  cBox.get());
+        FinishButton['text'] = "CheckOut";        
+    else:
+        Choice.insert(END,  ", " + cBox.get());
 
-# 1. Introduce a list variable keeps track of the user's choices
+def checkout():
+    if len(Choice.get("1.0", END)) != 1:
+        text_file = open("Groceries.txt", "w");
+        text_file.write("Groceries List:\n%s" % str(Choice.get('1.0', END)));
+        text_file.close();
 
-# 2. Add a button for saving the list
+        Choice.delete("1.0", END);
+        FinishButton['text'] = "Items Saved";
+        grossCombo.set("Please Select Yo Shizz");
 
-# 3. Define a function which writes the contents of the variable to
-#    a text file
+CartLabel = Label(window, height = 1, width = 30, font = ('Arial', 12), text = 'Cart');
+CartLabel.pack();
+# 2. Create a Text area to display the user's choice and
+#    pack it into the main window
+Choice = Text(window, height = 20, width = 30, font = ('Arial', 12));
+Choice.pack();
 
-
+# 3. Create a Combobox widget whose parent container is the
+#    main window and pack it
+grossCombo = Combobox(window, width = 20, value = groceries);
+grossCombo.set("Please Select Yo Shizz");
+grossCombo.state(['readonly']);
+grossCombo.bind("<<ComboboxSelected>>", lambda event:display_current_choice(grossCombo));
+grossCombo.pack();
+# 4. Create a Button to push when the user is happy with
+#    their choice and pack it
+FinishButton = Button(window, height = 1, width = 30, font = ('Arial', 12), text = 'CheckOut', command = checkout);
+FinishButton.pack();
+    
 # Start the event loop to react to user inputs
 window.mainloop()
