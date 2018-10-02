e830b34a97182d19f9b38b980eba1741bd511900
Togene <genejmartens@gmail.com>
2018-10-03 08:22:17 +1000

    Assesment 2 Added and worked on

--- Assesment2/the_best.py
+++ Assesment2/the_best.py
@@ -53,7 +53,7 @@ from urllib.request import urlopen
 # Import the standard Tkinter functions. (You WILL need to use
 # these functions in your solution.)
 from tkinter import *
-
+import os
 # Functions for finding all occurrences of a pattern
 # defined via a regular expression, as well as
 # the "multiline" and "dotall" flags.  (You do NOT need to
@@ -61,7 +61,7 @@ from tkinter import *
 # can be solved with the string "find" function, but it will
 # be difficult to produce a concise and robust solution
 # without using regular expressions.)
-from re import findall, finditer, MULTILINE, DOTALL
+from re import findall, finditer, MULTILINE, DOTALL, sub
 
 # Import the standard SQLite functions (just in case they're
 # needed).
@@ -136,17 +136,171 @@ def download(url = 'http://www.wikipedia.org/',
 #
 
 ##### DEVELOP YOUR SOLUTION HERE #####
+
+def CleanUpWhiteSpace(text):
+        for i in range(len(text)):
+            text[i] = text[i].strip();
+        return text;
+
+#using this as a text scrub to remove anything i dont want in text(&quot)
+def GeneralCleanUp(text):
+
+    for i in range(len(text)):
+        text[i] = text[i].replace("&quot", "");
+    return text;
+
+def PrintInformation(InformationBlock, limit = 10):
+    print("#-----------------------------------------#");
+    for i in range(len(InformationBlock)):
+
+        if i <= limit:
+            print(InformationBlock[i]);
+    print("#-----------------------------------------#");
+
+
+#download from source, or access archive
+def AcessBookInformation(downloadsource = False):
+    #Download Book Weekling Top, Grabbing names, now need to cull the list, and grab any relevant information (pictures, dates, links and so on)
+    #https://www.goodreads.com/book/most_read
+    bookreadingdownload = "";
+    html_file = "";
+    
+    if downloadsource:
+        print("Downloading......");
+        bookreadingdownload = download("https://www.goodreads.com/book/most_read", "download/bookreadingdownload");
+    else:
+        print("Accessing From Archive");
+        html_file = open("Archived/bookreadingdownload.html", 'r', encoding='utf-8');
+        bookreadingdownload = html_file.read();
+        
+    booktitles = findall("<span itemprop='name'>(.*?)</span>", bookreadingdownload);
+
+    #clean up and remove titles past 10
+    del booktitles[10:];
+    bookindex = findall('<td valign="top" class="number">(.*?)</td>', bookreadingdownload);
+    #PrintInformation(booktitles);
+
+    #Close File Read if were not downloading
+    if not(downloadsource):
+        html_file.close();
+
+    return booktitles;
+
+def AccessMusicInformation(downloadsource = False):
+    #http://www.bbc.co.uk/radio1/chart/singles
+
+    musictopdownload = "";
+    html_file = "";
+    
+    if downloadsource:
+        print("Downloading......");
+        musictopdownload = download("http://www.bbc.co.uk/radio1/chart/singles", "download/musictopdownload");
+    else:
+        print("Accessing From Archive");
+        html_file = open("Archived/musictopdownload.html", 'r', encoding='utf-8');
+        musictopdownload = html_file.read();
+        
+    musictitles = findall('<div class="cht-entry-title">(.*?)</div>', musictopdownload);
+    del musictitles[10:];
+    musicindex = findall('<div class="cht-entry-position">(.*?)</div>', musictopdownload);
+    #PrintInformation(musictitles);
+
+    #Close File Read if were not downloading
+    if not(downloadsource):
+        html_file.close();
+        
+    return musictitles;
+    
+def AccessElectronicInformation(downloadsource = False):
+    #https://www.amazon.com/Best-Sellers-Electronics/zgbs/electronics
+    electronicstopsaledownload = "";
+    html_file = "";
+    
+    if downloadsource :
+        print("Downloading......");
+        electronicstopsaledownload = download("https://www.amazon.com/Best-Sellers-Electronics/zgbs/electronics", "download/electronicstopsaledownload");
+    else:
+        print("Accessing From Archive");
+        html_file = open("Archived/electronicstopsaledownload.html", 'r', encoding='utf-8');
+        electronicstopsaledownload = html_file.read();
+
+
+    electronicstopsaletitle = findall('<div class="p13n-sc-truncate p13n-sc-line-clamp-2" aria-hidden="true" data-rows="2">(.*?)</div>', electronicstopsaledownload, DOTALL);
+    electronicstopsaletitle = CleanUpWhiteSpace(electronicstopsaletitle);
+    electronicstopsaletitle = GeneralCleanUp(electronicstopsaletitle);
+    del electronicstopsaletitle[10:];
+    electronicstopsaleindex = findall('<span class="zg-badge-text">(.*?)</span>', electronicstopsaledownload);
+    #PrintInformation(electronicstopsaletitle);
+
+    #Close File Read if were not downloading
+    if not(downloadsource):
+        html_file.close();
+
+    return electronicstopsaletitle;
+
+def Test():
+    print("Poo");
+
+def DisplayInformation(information):
+    new_window = Toplevel(window);
+
+    displayLabel = Label(new_window, font = ('Ariel', 12), justify=LEFT);
+    displayLabel.grid(row=1, column=2)
+
+    for i in range(len(information)):
+        displayLabel["text"] += "["+str(i + 1)+"]" +"  "+ (information[i] + "\n")
+        
 window = Tk();
 window.title("Spicy Data");
-window.geometry("500x300");
-#Download Book Weekling Top, Grabbing names, now need to cull the list, and grab any relevant information (pictures, dates, links and so on)
-#https://www.goodreads.com/book/most_read
-bookreadingdownload = download("https://www.goodreads.com/book/most_read", "download/bookreadingdownload");
-booktitles = findall("<span itemprop='name'>(.*?)</span>", bookreadingdownload);
-bookindex = findall('<td valign="top" class="number">(.*?)</td>', bookreadingdownload);
+window.geometry("600x300");
 
-for title in booktitles :
-    print(title);
-#print(findall("<span itemprop='name'>(.*?)</span>", bookreadingdownload), end = "");
+BookInformation = AcessBookInformation(False);
+MusicInformation = AccessMusicInformation(False);
+ElectronicInformation = AccessElectronicInformation(False);
+
+#Setting it up
+
+
+leftFrame = Frame(window, width = 200, height = 600);
+leftFrame.grid(row=0,column=0, padx=10,pady=2);
+
+rightFrame = Frame(window, width = 100, height = 5, borderwidth = 5);
+rightFrame.grid(row=0,column=1, padx=0,pady=0);
+
+img = PhotoImage(file = "images/tenor.gif");
+ChillImageLabel = Label(leftFrame, image = img).grid(row = 0, column = 0);
+
+
+MostReadFrame = LabelFrame(rightFrame, text="Most Read Books", width = 200, height = 3, borderwidth = 1, relief = GROOVE);
+MostReadFrame.grid(row=0,column=0, padx=0,pady=0, sticky=N);
+
+MostListenedPreviousRadial = Radiobutton(MostReadFrame, text="Previous", width = 5);
+MostListenedPreviousRadial.grid(row = 0, column = 1, padx=(0, 0), pady=(0, 0));
+
+MostListenedCurrentRadial= Radiobutton(MostReadFrame, text="Current", width = 5);
+MostListenedCurrentRadial.grid(row = 0, column = 2, padx=(0, 0), pady=(0, 0));
+
+MostListenframe = LabelFrame(rightFrame, text="Most Listened To Music", width = 200, height =3, borderwidth = 1, relief = GROOVE);
+MostListenframe.grid(row=1,column=0, padx=0,pady=0, sticky=N);
+
+MostListenedPreviousRadial = Radiobutton(MostListenframe, text="Previous", width = 5);
+MostListenedPreviousRadial.grid(row = 0, column = 1, padx=(0, 0), pady=(0, 0));
+
+MostListenedCurrentRadial= Radiobutton(MostListenframe, text="Current", width = 5);
+MostListenedCurrentRadial.grid(row = 0, column = 2, padx=(0, 0), pady=(0, 0));
+
+MostBoughtFrame = LabelFrame(rightFrame, text="Most Bought Electronics", width = 200, height =3, borderwidth = 1, relief = GROOVE);
+MostBoughtFrame.grid(row=2,column=0, padx=0,pady=0, sticky=N);
+
+MostListenedPreviousRadial = Radiobutton(MostBoughtFrame, text="Previous", width = 5);
+MostListenedPreviousRadial.grid(row = 0, column = 1, padx=(0, 0), pady=(0, 0));
+
+MostListenedCurrentRadial= Radiobutton(MostBoughtFrame, text="Current", width = 5);
+MostListenedCurrentRadial.grid(row = 0, column = 2, padx=(0, 0), pady=(0, 0));
+#PreviewButton.bind('<Button>', Test());
+#DisplayInformation(BookInformation);
+#DisplayInformation(MusicInformation);
+#DisplayInformation(ElectronicInformation);
+
 
 
